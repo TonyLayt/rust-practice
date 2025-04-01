964a6a9a45201cfad3a0d67d249a099639c80a61
Max_ideapad <makctony@ukr.net>
2025-04-01 22:29:30 +0300

    Created a Christmas tree drawing function

---
+++ src/homework/hw06.rs
@@ -0,0 +1,15 @@
+
+pub fn draw_tree(triangle_count: usize) {
+    // Проходимо по кожному трикутнику
+    for i in 0..triangle_count {
+        // Малюємо рядки для поточного трикутника
+        for j in 0..=i {
+            // Додаємо пробіли перед зірочками для вирівнювання
+            let spaces = " ".repeat(triangle_count - j - 1);
+            // Генеруємо рядок із зірочками
+            let stars = "*".repeat(2 * j + 1);
+            // Виводимо рядок у консоль
+            println!("{}{}", spaces, stars);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ fn main() {
     println!("We learn Rust! \n\n");
     //homework::hw03::draw_an_envelope();
     //homework::hw04::drawing_a_diamond();
-    homework::hw05::test();
+    //homework::hw05::test();
+    homework::hw06::draw_tree(6)
 }
